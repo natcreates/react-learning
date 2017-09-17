380fdf674a7ce59fed16724e101e5ec91a28e515
natcreates <natalie@nataliejdixon.com>
2017-09-17 17:56:58 +0100

    added notes

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import SearchBar from './search-bar';
+import VideoList from './video-list';
+// import BookList from '../containers/booklist';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+        <div>
+            <SearchBar />
+            <VideoList />
+        </div>
     );
   }
 }
