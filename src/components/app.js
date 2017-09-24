a315068b99742bdff623cfd4f8def837e4e5519b
unknown <natalie@nataliejdixon.com>
2017-09-24 23:01:36 +0100

    removed old video-app files in prep for new project

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import SearchBar from './search-bar';
-import VideoList from './video-list';
-// import BookList from '../containers/booklist';
 
 export default class App extends Component {
   render() {
     return (
         <div>
-            <SearchBar />
-            <VideoList />
         </div>
     );
   }
