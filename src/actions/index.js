5cc9f9b7f5cc77b3070d219fb0940c2476c360a8
natcreates <natalie.dixon@tesglobal.com>
2017-10-18 18:32:23 +0100

    added ability to delete posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const SAVE_POST = 'SAVE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const FETCH_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
@@ -34,5 +35,15 @@ export function fetchPost(id) {
     return {
         type: FETCH_POST,
         payload: request
+    };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+  .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: id
     };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,16 +1,19 @@
 import {mapKeys} from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions/index';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
 
 // mapKeys can take an array of objects and return a new object
 // using whatever is specified as the key
 const PostsReducer = (state = {}, action) => {
   switch(action.type) {
+    case DELETE_POST:
+    // return new state object without the post with this id (payload)
+      return _.omit(state, action.payload);
     case FETCH_POST:
     // keep all the existing posts we have in state using ...state
     // make a new key on the state obj with the val of the particular post
       return {...state, [action.payload.data.id]: action.payload.data};
     case FETCH_POSTS:
-      return mapKeys(action.payload.data, 'i  d');
+      return mapKeys(action.payload.data, 'id');
     default:
       return state;
   }
